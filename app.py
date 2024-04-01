915582ed3dd3818948295dd83a49fd9147558710
Rr210 <1577791638@qq.com>
2024-04-01 22:02:09 +0800

    init: 上传；

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+from gptapi.aitianhu import RequestHandler
+
+app = Flask(__name__)
+
+aitianhu_request_handler = RequestHandler()
+
+
+@app.route("/aitianhu", methods=["POST"])
+def process_request():
+    data = request.get_json()
+    response_text, status_code = aitianhu_request_handler.send_request(data)
+    return jsonify({"response_text": response_text, "status_code": status_code})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ gptapi/aitianhu.py
@@ -0,0 +1,43 @@
+import requests
+
+
+class RequestHandler:
+    def __init__(self):
+        self.url = "https://lhfjha.aitianhu1.top/api/please-donot-reverse-engineering-me-thank-you"
+        self.headers = {
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cache-Control": "no-cache",
+            "Content-Type": "application/json",
+            "Cookie": "sl-session=HFUGNlzzC2avL+jCw6uDbg==; sl_jwt_session=Wmw5NPCvCmZW2ppxrAkTXw==; cdn=aitianhu; SERVERID=srv99n2|Zgqiz",
+            "Origin": "https://lhfjha.aitianhu1.top",
+            "Pragma": "no-cache",
+            "Referer": "https://lhfjha.aitianhu1.top/",
+            "Sec-Ch-Ua": '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": '"Windows"',
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        }
+
+    def send_request(self, data):
+        try:
+            response = requests.post(
+                self.url,
+                json={
+                    "prompt": data["prompt"],
+                    "options": data["options"] | {},
+                    "model": "gpt-3.5-turbo",
+                    "OPENAI_API_KEY": "sk-AItianhuFreeForEveryone",
+                    "systemMessage": "你是一个人工智能助理，一个受过训练的大型语言模型。请仔细遵循用户的说明。使用markdown进行响应。",
+                    "temperature": 0.8,
+                    "top_p": 1,
+                },
+                headers=self.headers,
+            )
+            return response.text, response.status_code
+        except requests.exceptions.RequestException as e:
+            return str(e), 500
