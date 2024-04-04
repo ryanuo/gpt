33d80b32505545e7e52e714b0bfb60bf62d32599
Rr210 <1577791638@qq.com>
2024-04-04 14:33:23 +0800

    feat: adjust;

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from g4f.client import Client
 import g4f
+
+from .post.index import get_url_post_text, webhook_post
 from .gptapi.nexrachat import NexraChatAPI
 from .gptapi.aitianhu import RequestHandler
 
@@ -12,18 +14,83 @@ chat_api = NexraChatAPI()
 engine = g4f.client.Client()
 CORS(app, resources={r"/g4f/*": {"origins": "https://mr90.top"}})
 
+g4f_model_list = [
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-0613",
+    "gpt-3.5-turbo-16k",
+    "gpt-3.5-turbo-16k-0613",
+    "gpt-3.5-long",
+    "gpt-4",
+    "gpt-4-0613",
+    "gpt-4-32k",
+    "gpt-4-32k-0613",
+    "gpt-4-turbo",
+    "llama2-7b",
+    "llama2-13b",
+    "llama2-70b",
+    "codellama-34b-instruct",
+    "codellama-70b-instruct",
+    "gigachat",
+    "gigachat_plus",
+    "gigachat_pro",
+    "mixtral-8x7b",
+    "mistral-7b",
+    "dolphin-mixtral-8x7b",
+    "lzlv-70b",
+    "airoboros-70b",
+    "airoboros-l2-70b",
+    "openchat_3.5",
+    "gemini",
+    "gemini-pro",
+    "claude-v2",
+    "claude-3-opus",
+    "claude-3-sonnet",
+    "pi",
+]
 
-@app.route("/g4f/generate_completion", methods=["POST"])
-def generate_completion():
+
+@app.route("/g4f/<path:model>", methods=["POST"])
+def generate_completion(model):
+    if model not in g4f_model_list:
+        return jsonify({"error": "Invalid model"})
     # 获取请求中的用户消息内容
     message = request.json.get("message")
+
+    # 使用 g4f 客户端生成对话完成结果
+    completion = engine.chat.completions.create(model=model, messages=message)
+
+    # 获取对话完成结果中的内容并返回
+    completion_content = completion.choices[0].message.content
+    return jsonify({"data": completion_content, "status_code": 200})
+
+
+@app.route("/ai-post", methods=["POST"])
+def ai_post():
+    url = request.headers.get("Refererurl")
+    if not url:
+        return jsonify({"error": "Invalid url"})
+    text = get_url_post_text(url)
+    message = [
+        {
+            "role": "assistant",
+            "content": "概括以下内容,50个字数左右,不要超出文字字数限制",
+        },
+        {"role": "user", "content": text},
+    ]
 
     # 使用 g4f 客户端生成对话完成结果
     completion = engine.chat.completions.create(model="openchat_3.5", messages=message)
 
     # 获取对话完成结果中的内容并返回
     completion_content = completion.choices[0].message.content
-    return jsonify({"data": completion_content, "status_code": 200})
+    return jsonify(
+        {"data": completion_content, "status_code": 200, "model": "openchat_3.5"}
+    )
+
+
+@app.route("/webhook", methods=["GET"])
+def webhook():
+    return webhook_post()
 
 
 @app.route(f"/model/(.*?)/", methods=["POST"])
---
+++ api/post/index.py
@@ -0,0 +1,36 @@
+# 存储文章数据的 JSON 文件路径
+import json
+import os
+import requests
+
+json_file_path = os.path.join(os.path.dirname(__file__), "articles.json")
+
+
+def webhook_post():
+    # 获取 JSON 数据
+    data = requests.get("https://mr90.top/sitemap.json").json()
+    # 创建一个空的字典对象，用于存储 URL 和文本内容的映射关系
+    url_text_map = {}
+    # 遍历每个条目
+    for item in data["items"]:
+        # 提取 URL 和文本内容，并存储到字典中
+        url = item["url"]
+        text = item.get("text", "")  # 如果 text 字段不存在，则默认为空字符串
+        url_text_map[url] = text
+        # 将收到的 JSON 数据保存到文件中
+    with open(json_file_path, "w") as f:
+        json.dump(url_text_map, f, indent=2)
+        f.close()
+    return {
+        "message": "JSON url_text_map received and saved successfully.",
+        "code": 200,
+    }
+
+
+def get_url_post_text(url) -> str:
+    # 加载JSON文件并获取对应的文本
+    with open(json_file_path, "r") as f:
+        url_text_map = json.load(f)  # 加载JSON数据到字典
+        f.close()  # 显式关闭文件
+    text = url_text_map[url]  # 从字典中获取对应URL的文本
+    return text
