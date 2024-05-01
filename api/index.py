bdd2da2d0a22309d5af9fab7933cdcf41e0a34ec
Rr210 <1577791638@qq.com>
2024-05-01 11:20:36 +0800

    chore: update;

--- api/index.py
+++ api/index.py
@@ -5,50 +5,13 @@ from g4f.client import Client
 from .gptapi.wxchat import handle_text_message, verify_signature, subscribe_reply
 from wechatpy.exceptions import InvalidMchIdException
 from wechatpy import parse_message, create_reply
-from .post.index import get_url_post_text, webhook_post
-from .gptapi.nexrachat import NexraChatAPI
-from .gptapi.aitianhu import RequestHandler
+from .post.index import get_url_post_text
+from .config import g4f_model_list, default_model
 
 app = Flask(__name__)
 
-aitianhu_request_handler = RequestHandler()
-chat_api = NexraChatAPI()
 client = Client()
 CORS(app, resources={r"/*": {"origins": "https://mr90.top"}})
-
-g4f_model_list = [
-    "gpt-3.5-turbo",
-    "gpt-3.5-turbo-0613",
-    "gpt-3.5-turbo-16k",
-    "gpt-3.5-turbo-16k-0613",
-    "gpt-3.5-long",
-    "gpt-4",
-    "gpt-4-0613",
-    "gpt-4-32k",
-    "gpt-4-32k-0613",
-    "gpt-4-turbo",
-    "llama2-7b",
-    "llama2-13b",
-    "llama2-70b",
-    "codellama-34b-instruct",
-    "codellama-70b-instruct",
-    "gigachat",
-    "gigachat_plus",
-    "gigachat_pro",
-    "mixtral-8x7b",
-    "mistral-7b",
-    "dolphin-mixtral-8x7b",
-    "lzlv-70b",
-    "airoboros-70b",
-    "airoboros-l2-70b",
-    "openchat_3.5",
-    "gemini",
-    "gemini-pro",
-    "claude-v2",
-    "claude-3-opus",
-    "claude-3-sonnet",
-    "pi",
-]
 
 
 @app.route("/g4f/<path:model>", methods=["POST"])
@@ -81,33 +44,13 @@ def ai_post():
     ]
 
     # 使用 g4f 客户端生成对话完成结果
-    completion = client.chat.completions.create(model="gpt-3.5-turbo", messages=message)
+    completion = client.chat.completions.create(model=default_model, messages=message)
 
     # 获取对话完成结果中的内容并返回
     completion_content = completion.choices[0].message.content
     return jsonify(
-        {"data": completion_content, "status_code": 200, "model": "gpt-3.5-turbo"}
+        {"data": completion_content, "status_code": 200, "model": default_model}
     )
-
-
-@app.route("/webhook", methods=["GET"])
-def webhook():
-    return webhook_post()
-
-
-@app.route(f"/model/(.*?)/", methods=["POST"])
-def send_request():
-    model = request.url.split("/")[4]  # Extract the model from the URL
-    data = request.get_json()
-
-    return jsonify(NexraChatAPI.send_message(messages=data, model=model))
-
-
-@app.route("/aitianhu", methods=["POST"])
-def process_request():
-    data = request.get_json()
-    response_text, status_code = aitianhu_request_handler.send_request(data)
-    return jsonify({"response_text": response_text, "status_code": status_code})
 
 
 @app.route("/", methods=["GET"])
--- api/post/index.py
+++ api/post/index.py
@@ -1,30 +1,4 @@
-# 存储文章数据的 JSON 文件路径
-import json
-import os
 import requests
-
-json_file_path = os.path.join(os.path.dirname(__file__), "articles.json")
-
-
-def webhook_post():
-    # 获取 JSON 数据
-    data = requests.get("https://mr90.top/sitemap.json").json()
-    # 创建一个空的字典对象，用于存储 URL 和文本内容的映射关系
-    url_text_map = {}
-    # 遍历每个条目
-    for item in data["items"]:
-        # 提取 URL 和文本内容，并存储到字典中
-        url = item["url"]
-        text = item.get("text", "")  # 如果 text 字段不存在，则默认为空字符串
-        url_text_map[url] = text
-        # 将收到的 JSON 数据保存到文件中
-    with open(json_file_path, "w") as f:
-        json.dump(url_text_map, f, indent=2)
-        f.close()
-    return {
-        "message": "JSON url_text_map received and saved successfully.",
-        "code": 200,
-    }
 
 
 def get_url_post_text(url) -> str:
